b1fe7ada87f29c026bffcbc551a2eed1927cd27b
ZorsMD <145523438+ZorsMD@users.noreply.github.com>
2023-10-12 22:57:07 +0700

    Alexia-MD

---
+++ settings.js
@@ -0,0 +1,52 @@
+/*
+   Created By ArxzyDev
+   My Contact wa.me/6289513081052
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+/* ~~~~~~~~~ WEB API ~~~~~~~~~ */
+global.lol = '' // https://api.lolhuman.xyz
+global.xzn = '' // https://xnz.wtf
+/* ~~~~~~~~~ SETTINGS OWNER ~~~~~~~~~ */
+global.numberowner = '6283892216115' // Owner Utama
+global.owner = ['6283892216115', '6283892216115'] // Owner Lainnya
+global.namaowner = 'Zors お-ぎ' // Nama Owner
+global.premium = ["6283892216115"] // Premium User
+global.nobot = '628561005249'
+/* ~~~~~~~~~ SETTINGS BOT ~~~~~~~~~ */
+global.namabot = 'Alexia-MD' // NickBot
+global.typemenu = 'v2' // 'v1' > 'v2' > 'v3' > 'v4'
+global.typereply = 'v1' // 'v1' > 'v2'
+global.autoread = true // ReadChat
+global.autobio = false // AutoBio
+global.autoblok212 = true // AutoBlock Nomer +212
+global.onlyindo = false  // AutoBlock Selain Nomer Indo
+global.packname = 'Created By' // Watermark Sticker
+global.author = 'Zors ダ' // Watermark Sticker
+/* ~~~~~~~~~ MESSAGES ~~~~~~~~~ */
+global.mess = {
+    done: 'Done ✅',
+    prem: 'Feature Only For User _*PREMIUM*_',
+    admin: 'Feature Only for _*Admin Group*_',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: 'Feature Only for _*owner*_',
+    group: 'Feature Only for _*Group Chat*_',
+    private: 'Feature Only for _*Private Chat*_',
+    wait: 'Wait a Moment, for Process',    
+    error: '_*Sorry Features Error*_',
+}
+/* ~~~~~~~~~ THUMBNAIL ~~~~~~~~~ */
+global.thumb = fs.readFileSync('./media/quoted.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
+/* ~~~~~~~~~ EDITS LINK ~~~~~~~~~ */
+global.link = 'https://linkbio.co/ZorsMD'
+/* ~~~~~~~~~ END SYSTEM ~~~~~~~~~ */
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
